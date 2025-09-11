fccf458516029aad44575d42d058dc450f8a8647
Akrila <110743707+AkrilaMayNotBeAvailable@users.noreply.github.com>
2025-09-11 01:10:56 -0300

    Atualização Exerc 2 Java

--- Lab1/Exercicio2.java
+++ Lab1/Exercicio2.java
@@ -1,11 +1,9 @@
 //Codigos feitos pro Douglas e Jorge
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 class SimpleThread extends Thread {
-	
 	private Integer id;
 	
 	public SimpleThread(Integer id) {
@@ -13,11 +11,8 @@ class SimpleThread extends Thread {
 	}
 
 	public void run() {
-
-		
-
 		Random r = new Random();	
-		try { Thread.currentThread().sleep(r.nextInt(301));}
+		try { Thread.sleep(r.nextInt(301));}
 		catch (InterruptedException ie) { ie.printStackTrace(); }
 			
 		System.out.println("FIM "+ id);
@@ -25,30 +20,19 @@ class SimpleThread extends Thread {
 }
 
 public class Exercicio2 {
-	
 	public static void main(String args[]) {
-		
-		//System.out.println(args[0]);
 		List<SimpleThread> threads = new ArrayList<>();
 		for (int i = 0; i < (Integer.parseInt(args[0])); i++) 
 		{
 			SimpleThread temp = new SimpleThread(i);
 			threads.add(temp);
-			temp.start();
-			
+			temp.start();	
 		}
-		//SimpleThread thr1 =  new SimpleThread("Joe");
-		//SimpleThread thr2 =  new SimpleThread("Paul");
-		
-		//thr1.start();
-		//thr2.start();
-		
+
 		try {
 			for (SimpleThread targetTh : threads) {
 				targetTh.join();
 			}
-			//thr1.join();
-			//thr2.join();
 		} catch (InterruptedException ie) { 
 			ie.printStackTrace(); 
 		}
